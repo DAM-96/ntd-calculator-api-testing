c74788e10b29cd07ba2bbb3e185b18f2e9661eb2
Daniel Moreno <d.a.morenog@outlook.com>
2024-06-03 14:09:15 -0600

    Added variables to Cucumber World and created calculatorSteps.js

--- utils/hooks.js
+++ utils/hooks.js
@@ -1,19 +1,20 @@
 const { Before } = require('@cucumber/cucumber');
 const { execSync } = require('child_process');
 
-let isBeforeAllExecuted = false;
-
 Before(async function () {
-    if(!isBeforeAllExecuted) {
-        console.log('Performing initial Docker setup...');
-        execSync('docker pull public.ecr.aws/l4q9w4c5/loanpro-calculator-cli:latest');
-        isBeforeAllExecuted = true;
-    }
+    this.isDockerImageValidated = false;
 
     this.operations = [
         "add",
         "subtract",
         "multiply",
         "divide"
-    ]
+    ];
+
+    if(!this.isDockerImageValidated) {
+        console.log('Performing initial Docker setup...');
+        execSync('docker pull public.ecr.aws/l4q9w4c5/loanpro-calculator-cli:latest');
+        this.isDockerImageValidated = true;
+    }
+
 });
